0b9f45ecf910d3afc00d0d092e22952fd8187e64
Victoria Waltz <148576621+Victoria-Waltz@users.noreply.github.com>
2025-10-02 15:09:10 -0400

    Created launcher for new layout to change text sizes

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,14 +1,24 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
+
+const val TEXT_SIZE = "textsize"
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
-
+    private val textSizeLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == RESULT_OK){
+            val size = it.data?.getFloatExtra(TEXT_SIZE,16f) ?: 16f
+        lyricsDisplayTextView.textSize = size
+    }
+    }
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
 
     private lateinit var lyricsDisplayTextView: TextView
@@ -20,6 +30,11 @@ class DisplayActivity : AppCompatActivity() {
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
+
+        textSizeSelectorButton.setOnClickListener {
+            val intent = Intent(this@DisplayActivity, TextSizeActivity::class.java)
+            textSizeLauncher.launch(intent)
+        }
 
     }
 }
--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,13 +24,15 @@ class TextSizeActivity : AppCompatActivity() {
         with (findViewById(R.id.textSizeSelectorRecyclerView) as RecyclerView) {
 
             // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
-            adapter = TextSizeAdapter(textSizes){
-
+            adapter = TextSizeAdapter(textSizes){ selectedSize ->
+                val resultIntent = Intent().apply{
+                    putExtra(TEXT_SIZE,selectedSize.toFloat())
+                }
+                setResult(RESULT_OK, resultIntent)
+                finish()
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
         }
-
-
 
     }
 }
